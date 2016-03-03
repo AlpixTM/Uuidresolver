b1ab9f1904e61239e8ad9430e532fedf37af9c77
AlpixYT <Lukasniekum@googlemail.com>
2016-03-03 20:39:03 +0100

    Create uuidresolver.php
    Nutzung
    (Außerhalb auf anderen .php Scripten z.B.: index,php)
    include_once 'uuidresolver.php';
    $resolver=new resolver(); 
    $uuid=$resolver->resolveuuidlong("AlpixYT");
    echo $uuid;
    $uuid=$resolver->resolveuuidshort("AlpixYT");
    echo $uuid;
    
    Soltest du vergessen einen Nutzernamen anzugeben, wird automatisch Alpix genutzt, um Fehler zu vermeiden :]

---
+++ uuidresolver.php
@@ -0,0 +1,34 @@
+<?php
+// ->Alpix
+// 03.03.16
+class resolver {
+	function resolveuuidshort($username) {
+		if (!$username){
+			$username="AlpixYT";
+		}
+		$url = "https://api.mojang.com/users/profiles/minecraft/" . urlencode($username);
+		$content = file_get_contents($url); 
+		$json = json_decode($content,true);
+		return $json['id'];
+	}
+	function resolveuuidlong($username) {
+		if (!$username){
+			$username="AlpixYT";
+		}
+		$url = "https://api.mojang.com/users/profiles/minecraft/" . urlencode($username);
+		$content = file_get_contents($url);
+		$json = json_decode($content,true);
+		$uuid=$json['id'];
+		$longuuid = substr($uuid,0,8).'-'.substr($uuid,8,4).'-'.substr($uuid,12,4).'-'.substr($uuid,16,4).'-'.substr($uuid,20,12);
+		return $longuuid;
+		
+	}
+}
+?>
+<?php
+$resolver=new resolver();
+$uuid=$resolver->resolveuuidlong("AlpixYT");
+echo $uuid;
+$uuid=$resolver->resolveuuidshort("AlpixYT");
+echo $uuid;
+?>
