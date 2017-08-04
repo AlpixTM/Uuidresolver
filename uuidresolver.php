1571027183dca2272f9d7c7d649aaaedc2f14cbd
AlpixTM <contact@alpix.eu>
2017-08-04 11:57:30 +0200

    + Resolve short and long ID

--- uuidresolver.php
+++ uuidresolver.php
@@ -1,6 +1,7 @@
 <?php
 // ->Alpix
 // 03.03.16
+// Update 04.08.2017
 class resolver {
 	function resolveuuidshort($username) {
 		if (!$username){
@@ -23,6 +24,22 @@ class resolver {
 		return $longuuid;
 		
 	}
+	function resoveuuidboth($username){
+        if (!$username){
+            $username="AlpixYT";
+        }
+        $url = "https://api.mojang.com/users/profiles/minecraft/" . urlencode($username);
+        $content = file_get_contents($url);
+        $json = json_decode($content,true);
+        $uuid= $json['id'];
+        $longuuid = substr($uuid,0,8).'-'.substr($uuid,8,4).'-'.substr($uuid,12,4).'-'.substr($uuid,16,4).'-'.substr($uuid,20,12);
+        $shortuuid = $uuid;
+        $uuids = array(
+                "short" => $shortuuid,
+                "long" => $longuuid,
+        );
+        return $uuids;
+    }
 }
 ?>
 //example
@@ -31,5 +48,7 @@ $resolver=new resolver();
 $uuid=$resolver->resolveuuidlong("AlpixYT");
 echo $uuid;
 $uuid=$resolver->resolveuuidshort("AlpixYT");
+echo $uuid;
+$uuid = $resolver->resoveuuidboth("AlpixYT");
 echo $uuid;
 ?>
